65878e38b534c3d77b7eea17396fd57b5ea83ee2
AceLikesGhosts <acelikesghosts@gmail.com>
2024-08-23 15:47:23 -0700

    update

--- plugins/PlatformSpoofer/patches/PropertyManager.ts
+++ plugins/PlatformSpoofer/patches/PropertyManager.ts
@@ -2,14 +2,11 @@ import PlatformSpoofer, { logger, meta } from '..';
 import { PropertiesToSpoofAs, propertyStuff } from '../utils';
 
 export default (): void => {
-    logger.log('Patching ProperyManager (getSuperProperties');
-    logger.log(propertyStuff, 'getSuperProperties', propertyStuff['getSuperProperties']);
+    logger.log('Patching PropertyManager (getSuperProperties');
 
     BdApi.Patcher.instead(meta.name, propertyStuff, 'getSuperProperties', (context, args, orig) => {
         const data = orig.apply(context, args);
 
-        logger.log('called getSuperProperties', data);
-        logger.log('finished');
         return {
             ...data,
             browser: PropertiesToSpoofAs[PlatformSpoofer.settings?.type]?.browser,
